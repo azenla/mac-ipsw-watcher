aa3f5c6de70d350b394199f87598656141ec8cd9
Kenneth Endfinger <kaendfinger@gmail.com>
2022-03-20 23:42:16 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mac-ipsw-watcher
+
+Use Virtualization.framework to continuously download and save the latest IPSW for macOS restore images.
--- mac-ipsw-watcher/main.swift
+++ mac-ipsw-watcher/main.swift
@@ -5,7 +5,46 @@
 //  Created by Kenneth Endfinger on 3/20/22.
 //
 
+import CommonCrypto
 import Foundation
+import Virtualization
 
-print("Hello, World!")
+func checkAndDownload() async throws {
+    let latestSupportedImage = try await VZMacOSRestoreImage.latestSupported
+    let url = latestSupportedImage.url
+    let absoluteUrlString = url.absoluteString
+    guard let ipswFileName = url.pathComponents.last else {
+        throw URLError(.badURL)
+    }
+    
+    guard let shaOfUrl = absoluteUrlString.data(using: .utf8)?.sha256() else {
+        throw URLError(.badURL)
+    }
+    let localFilePath = "\(shaOfUrl)/\(ipswFileName)"
+    if FileManager.default.fileExists(atPath: localFilePath) {
+        print("Latest IPSW is already downloaded: \(localFilePath)")
+        return
+    }
+    let localFileUrl = URL(fileURLWithPath: localFilePath)
+    try FileManager.default.createDirectory(at: localFileUrl.deletingLastPathComponent(), withIntermediateDirectories: true)
+    print("Downloading IPSW \(absoluteUrlString) to \(localFilePath)")
+    let (temporaryFileUrl, response) = try await URLSession.shared.download(from: url)
+    if (response as! HTTPURLResponse).statusCode != 200 {
+        throw URLError(.badURL)
+    }
+    try FileManager.default.moveItem(at: temporaryFileUrl, to: localFileUrl)
+    print("Latest IPSW \(localFilePath) downloaded.")
+}
 
+Task {
+    while true {
+        do {
+            try await checkAndDownload()
+        } catch {
+            print("ERROR: \(error)")
+        }
+        sleep(5)
+    }
+}
+
+dispatchMain()
